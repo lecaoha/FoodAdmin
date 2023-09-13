6a63b5f52ce1cf202856936e739b41a4567dab16
Huynhtanhung2002 <85001998+Huynhtanhung2002@users.noreply.github.com>
2023-09-13 18:28:54 +0700

    ha_fix

--- BackEnd/plugin/include/slidebar.php
+++ BackEnd/plugin/include/slidebar.php
@@ -18,8 +18,8 @@
           Tài Khoản
         </a>
         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="accountDropdown">
-          <li><a class="dropdown-item" href="./page_users.php">Khách Hàng</a></li>
-          <li><a class="dropdown-item" href="./page_employee.php">Nhân Viên</a></li>
+        <li><a class="dropdown-item" href="./page_users.php">Khách Hàng</a></li>
+          <li><a class="dropdown-item" href="#">Nhân Viên</a></li>
         </ul>
       </li>
 
